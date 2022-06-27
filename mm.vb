37094b4224c20e4208a15bdfa82fd8b803342bf4
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-06-26 20:57:09 -0700

    Update mm.vb

--- mm.vb
+++ mm.vb
@@ -1,3 +1,45 @@
+Public Class Form4
+
+    Public Declare Sub Sleep Lib "Kernel32" (ByVal dwMilliseconds As Long)
+    Public Declare Function mciSendString Lib "winmm.dll" Alias "mciSendStringA" (ByVal lpstyCommand As String, ByVal lpstrReturnString As String, ByVal uReturnLenght As Integer, ByVal hwndCallback As Integer) As Integer
+
+    Public Sub New()
+        InitializeComponent()
+
+        Dim A As Threading.Thread = New Threading.Thread(AddressOf taskA)
+        A.Start()
+    End Sub
+
+    Sub taskA()
+        While True
+            Label2.Text = Format(Now, "yyyy-MM-dd hh:mm:ss")
+        End While
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim port As String
+        Dim returnStr As String
+        Dim conn As IO.Ports.SerialPort
+        Dim availablePorts() As String
+        Dim startRecord As Integer
+        Dim statusRecord As Boolean = True
+
+        conn = New IO.Ports.SerialPort()
+        conn.PortName = com.Text.ToString()
+        conn.BaudRate = 9600
+        conn.DataBits = 8
+        conn.Parity = IO.Ports.Parity.None
+
+        conn.Close()
+        conn.Open()
+
+
+
+        Dim max As Integer = Convert.ToInt32(TextBox1.Text)
+
+        For i As Integer = 0 To max
             Dim se As Integer = Now().Second
             Dim mi As Integer = Now().Minute
             Dim ho As Integer = Now().Hour.ToString()
@@ -28,8 +70,8 @@
             End If
 
             Dim month() As String = {"Januari", "Pebruari", "Maret", "April", "Mei", "Juni",
-                                 "Juli", "Agustus", "September", "Oktober", "Nopember",
-                                 "Desember"}
+                                     "Juli", "Agustus", "September", "Oktober", "Nopember",
+                                     "Desember"}
 
             Dim folder As String = "D:\vb_database\" + Form2.username.Text.ToString() + "\" + ye + "\" + month(mo - 1) + "\" + da + "\"
             Dim datename As String = Sho + "-" + Smi + "-" + Sse
@@ -61,3 +103,6 @@
             End If
 
             Sleep(1000)
+        Next
+    End Sub
+End Class
